ec4097df5c600da228ce28e199626af0db65d2dd
Gabor Suhai <suhai.gabor@gmail.com>
2023-07-08 13:31:14 +0200

    Add Unicode support to K3 Pro

--- keyboards/keychron/k3_pro/ansi/white/keymaps/via/keymap.c
+++ keyboards/keychron/k3_pro/ansi/white/keymaps/via/keymap.c
@@ -16,12 +16,23 @@
 
 #include QMK_KEYBOARD_H
 
+enum unicode_names {
+    HU_AACU,
+    HU_AACU_CAP
+};
+
+const uint32_t unicode_map[] PROGMEM = {
+    [HU_AACU]  = 0x00E1,  // á
+    [HU_AACU_CAP]  = 0x00C1,  // Á
+};
+
 // clang-format off
 enum layers{
   MAC_BASE,
   MAC_FN,
   WIN_BASE,
-  WIN_FN
+  WIN_FN,
+  HU_DIAC
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -47,13 +58,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
      KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,             KC_HOME,
      KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,    KC_END,
-     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 KC_RALT, MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
+     KC_LCTL,  KC_LGUI,  KC_LALT,                                KC_SPC,                                 MO(HU_DIAC), MO(WIN_FN),KC_RCTL,  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
 [WIN_FN] = LAYOUT_ansi_84(
      KC_TRNS,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  BL_DOWN,  BL_UP,    KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_TRNS,  KC_TRNS,  BL_TOGG,
      KC_TRNS,  BT_HST1,  BT_HST2,  BT_HST3,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
      BL_TOGG,  BL_STEP,  BL_UP,    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
      KC_TRNS,  KC_TRNS,  BL_DOWN,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
-     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  BAT_LVL,  NK_TOGG,  KC_TRNS,  KC_TRNS,  UC_LINX,  UC_WINC,            KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+
+[HU_DIAC] = LAYOUT_ansi_84(
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+     KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,
+     KC_TRNS,  XP(HU_AACU, HU_AACU_CAP),  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,            KC_TRNS,
+     KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,            KC_TRNS,  KC_TRNS,  KC_TRNS,
      KC_TRNS,  KC_TRNS,  KC_TRNS,                                KC_TRNS,                                KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS)
 };
--- keyboards/keychron/k3_pro/readme.md
+++ keyboards/keychron/k3_pro/readme.md
@@ -19,3 +19,38 @@ Flashing example for this keyboard:
 **Reset Key**: Connect the USB cable, toggle mode switch to "Off", hold down the *Esc* key or reset button underneath space bar, then toggle then switch to "Cable".
 
 See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
+
+
+## Adding Unicode support
+
+I mean adding Hungarian characters. Read QMK's [Unicode](https://docs.qmk.fm/#/feature_unicode?id=setting-the-input-mode) 
+documentation. Because of lower and upper case characters it's best to use the `Unicode Map` feature. I added the á character 
+as an example to [keymap.c](ansi/white/keymaps/via/keymap.c).  
+I also added a 5th layer to contain the Hungarian characters and mapped the `R_ALT` key on the 4th layer to momentary trigger
+this new 5th layer.
+
+## Building the firmware
+
+The easiest to do it with [Docker](https://docs.qmk.fm/#/getting_started_docker?id=docker-quick-start). 
+
+## Flashing
+
+The keyboard must be put into bootloader mode by switching it `Off` and while holding down the *Esc* key switching it to 
+`Cable` mode. If the keyboard is in bootloader mode then the `lsusb` command with list a new item: 
+`0483:df11 STMicroelectronics STM Device in DFU Mode`.
+
+Flash won't work from Docker without proper `udev` rules on the host machine, or without `sudo`. 
+But with the `util/docker_cmd.sh` we can step into the docker container and execute any command from there. 
+So to flash from within the container we can execute this: 
+
+```bash
+dfu-util -a 0 -d 0483:DF11 -s 0x8000000:leave -D K3-Pro-ANSI-White-v1.00.bin
+```
+This command is written [here](https://www.keychron.com/blogs/archived/k3-pro-factory-reset-and-firmware-flash) and [here](https://docs.qmk.fm/#/flashing?id=stm32apm32-dfu).
+
+
+## Problems
+
+- during flashing sometimes an error occurs (`bytesdfu-util: Error during special command "ERASE_PAGE" get_status`).
+ Just try again until it works.
+- VIA won't load, `Fetching v3 definition failed`. See [here](https://www.reddit.com/r/Keychron/comments/13nmnph/received_invalid_protocol_version_from_device_and/), [here](https://www.reddit.com/r/Keychron/comments/13e707o/k3_pro_having_issues_in_via_when_compiling_my_own/) or [here](https://www.reddit.com/r/Keychron/comments/13e707o/comment/jkltm6h/).
